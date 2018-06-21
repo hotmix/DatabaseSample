88761bd0c38f66ddb163b45f5e9c81a2b6afcf24
hotmix <dai@hotmix.jp>
2018-06-21 10:27:17 +0900

    add Layout

--- app/src/main/java/jp/hotmix/databasesample/CocktailMemoActivity.java
+++ app/src/main/java/jp/hotmix/databasesample/CocktailMemoActivity.java
@@ -2,12 +2,48 @@ package jp.hotmix.databasesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CocktailMemoActivity extends AppCompatActivity {
+
+    int _cocktailId = -1;
+    String _cocktailName = "";
+    TextView _tvCocktailName;
+    Button _btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail_memo);
+
+        _tvCocktailName = findViewById(R.id.tvCocktailName);
+        _btnSave = findViewById(R.id.btnSave);
+        ListView lvCocktail = findViewById(R.id.lvCocktail);
+        lvCocktail.setOnItemClickListener(new ListItemClickListener());
+
+    }
+
+    public void onSaveButtonClick(View view){
+        EditText etNote = findViewById(R.id.etNote);
+        _tvCocktailName.setText(getString(R.string.tv_name));
+        etNote.setText("");
+        _btnSave.setEnabled(false);
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            _cocktailId = position;
+            _cocktailName = (String) parent.getItemAtPosition(position);
+            _tvCocktailName.setText(_cocktailName);
+            _btnSave.setEnabled(true);
+        }
     }
 }
